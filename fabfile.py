64184ddcedea2b77e7acd0c31c2a11c7f4998490
Aditya KD <aditya@adityakd.me>
2014-04-24 14:27:14 +0530

    Add fabfile

---
+++ fabfile.py
@@ -0,0 +1,25 @@
+import os
+from fabric.api import local, settings, abort, run, cd, lcd, sudo
+
+def commit():
+    with settings(warn_only=True):
+        local("git add -A . && git commit")
+
+def push():
+    local("git push origin master")
+
+def prepare_deploy():
+    commit()
+    push()
+
+def deploy():
+    code_dir = '/webapps/checker/PurchaseCheck'
+    with settings(warn_only=True):
+        if run("test -d %s" % code_dir).failed:
+            # For first deploy
+            run("git clone git@github.com:caughtinflux/caughtinflux.github.io.git %s" % code_dir)
+
+    with cd(code_dir):
+        run("git pull")
+        sudo("service nginx restart")
+
